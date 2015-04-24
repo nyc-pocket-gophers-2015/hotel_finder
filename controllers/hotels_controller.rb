56655a9dda5bdf2cbca9f9a2117e5ba492b44974
echenique11 & HanuMaIV <pair+echenique11.hanumaiv@devbootcamp.com>
2015-04-24 16:19:08 -0400

    uploading controller

--- controllers/hotels_controller.rb
+++ controllers/hotels_controller.rb
@@ -1,6 +1,7 @@
 require_relative "../models/list.rb"
 require_relative "../models/hotel.rb"
 require_relative "../helpers/parser.rb"
+require_relative "../view.rb"
 
 class HotelController
 
@@ -40,6 +41,7 @@ class HotelController
     when "1" then get_user_inputs
     when "q" then return
     else
+      case option_selected
       when "name" then list.order_by_name
       when "rating" then list.order_by_rating
       when "price high" then list.order_by_price_hi
@@ -55,5 +57,5 @@ end
 
 
 runner = HotelController.new
-runner.load("Hemet","CA")
-puts runner.list.hotels.first.name
+# runner.load("Hemet","CA")
+runner.setup
--- models/hotel.rb
+++ models/hotel.rb
@@ -10,7 +10,7 @@ class Hotel
   end
 
   def to_s
-    "Name: #{name}, Rating: #{hotelRating}, Street Address: #{address1}, Price Range: #{lowRate}-#{highRate}"
+    "Name: #{name}, Rating: #{@hotelRating}, Street Address: #{@address1}, Price Range: #{@lowRate}-#{@highRate}"
   end
 end
 
