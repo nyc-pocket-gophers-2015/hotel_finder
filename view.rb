9e933b4f9f0c7170a1bfb448f44f55400c95cdb9
AlexTaber & Aristides Perez <pair+alextaber.aperezmontan@devbootcamp.com>
2015-04-24 16:55:51 -0400

    random colors

--- view.rb
+++ view.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class View
 
   class << self
@@ -7,33 +9,33 @@ class View
     end
 
     def display_welcome
-      puts "Welcome to our SUPER FANTASTIC HOTEL FINDER !!"
+      puts "Welcome to our SUPER FANTASTIC HOTEL FINDER !!".colorize(rand_color)
     end
 
     def display_menu_options
       display_banner
-      puts "Type 1 to start a new search"
-      puts "Type 'q' to exit program"
+      puts "Type 1 to start a new search".colorize(rand_color)
+      puts "Type 'q' to exit program".colorize(rand_color)
       display_banner
     end
 
     def display_ask_for_city
-      puts "Which city are you staying in ?"
+      puts "Which city are you staying in ?".colorize(rand_color)
     end
 
     def display_ask_for_state(city)
-      puts "...and in what state is #{city}?"
+      puts "...and in what state is #{city}?".colorize(rand_color)
     end
 
     def display_sort_options
-      puts "Type 'Name' to sort list alphabetically"
-      puts "Type 'Rating' to sort list by rating"
-      puts "Type 'Price High' to sort list by most expensive"
-      puts "Type 'Price Low' to sort list by least expensive"
+      puts "Type 'Name' to sort list alphabetically".colorize(rand_color)
+      puts "Type 'Rating' to sort list by rating".colorize(rand_color)
+      puts "Type 'Price High' to sort list by most expensive".colorize(rand_color)
+      puts "Type 'Price Low' to sort list by least expensive".colorize(rand_color)
     end
 
     def display_current_page(page)
-      puts "Displaying page #{page}"
+      puts "Displaying page #{page}".colorize(rand_color)
     end
 
     def get_input
@@ -41,12 +43,17 @@ class View
     end
 
     def display_error(error)
-      puts "Sorry, I didn't understand... \n#{error}. \nPlease try again:"
+      puts "Sorry, I didn't understand... \n#{error}. \nPlease try again:".colorize(rand_color)
     end
 
     def display_banner
-      40.times { print '*' }
+      40.times { print '*'.colorize(rand_color) }
       puts
+    end
+
+    def rand_color
+      my_colors = [:red, :green, :blue, :yellow,:cyan]
+      my_colors[rand(0..my_colors.length)]
     end
   end
 
